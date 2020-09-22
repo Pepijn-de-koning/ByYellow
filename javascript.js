241052b30df8fb74d0bb91a3a5b8b85f566b7370
Rowan van Zijl <rd.vanzijl@gmail.com>
2020-09-22 11:31:00 +0200

    Styling aanpassingen en hamburger icoon scroll styling

--- javascript.js
+++ javascript.js
@@ -1,1 +1,28 @@
  AOS.init();
+
+const logo = document.getElementById("hamburger");
+const images = document.getElementById("video");
+
+function isInsideInImages(images, logoPos) {
+    let imagePos = images.getBoundingClientRect();
+
+    if (
+      logoPos.top <= imagePos.top + imagePos.height &&
+      logoPos.top + logoPos.height > imagePos.top
+    ) {
+      return true;
+    }
+  
+  return false;
+}
+
+window.addEventListener("scroll", function () {
+  const a = logo.getBoundingClientRect();
+
+  if (isInsideInImages(images, a)) {
+    logo.classList.add("yellowmenu");
+  } else {
+    logo.classList.remove("yellowmenu");
+  }
+});
+
