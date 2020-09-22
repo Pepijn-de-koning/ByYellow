b35fa37ea39a0df391ff6c2e61d9ff5404e8a4c7
Rowan van Zijl <rd.vanzijl@gmail.com>
2020-09-22 12:35:38 +0200

    Fixed animation performance

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     document.body.classList.remove("loading");
-    console.log("LOADED!");
+    window.scrollTo(window.scrollX, window.scrollY - 1);
+    window.scrollTo(window.scrollX, window.scrollY + 1);
 }; 
 
 
@@ -35,15 +36,6 @@ var isInViewport = function (elem) {
       (window.innerWidth || document.documentElement.clientWidth)
   );
 };
-
-    var tl = gsap.timeline().to(".e", {
-      x: "random(0, 500, 5)", //chooses a random number between -20 and 20 for each target, rounding to the closest 5!
-      y: "random(0, 100, 3)",
-      duration: 1.5,
-      ease: "none",
-      repeat: -1,
-      repeatRefresh: true, // gets a new random x and y value on each repeat
-    });
 
 window.addEventListener("scroll", function () {
   const a = logo.getBoundingClientRect();
