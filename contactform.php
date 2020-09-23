e1677d67b8373866701f26252f4c0d0bb471b6d4
Teun van der Ploeg <29253@ma-web.nl>
2020-09-23 23:52:47 +0200

    contact formulier online
    contact formulier online

--- contactform.php
+++ contactform.php
@@ -1,35 +1,80 @@
 <?php
-ini_set('display_errors', 1);
+// ini_set('display_errors', 1);
 
-if (isset($_POST['submit'])) {
+// if (isset($_POST['submit'])) {
+
+//     $firstname = $_POST['firstname'];
+//     $lastname = $_POST['lastname'];
+//     $visitor_email = $_POST['email'];
+//     $message = $_POST['subject'];
+
+//     $email_body = "Gebruikers naam: $firstname' '$lastname\n" .
+//         "Gebruikers Email: $visitor_email\n" .
+//         "Bericht: $message.\n";
+
+
+//     $to = "29118@ma-web.nl";
+
+//     $headers = "From: $firstname \r\n";
+//     $headers = "From: $lastname \r\n";
+//     $headers = "From: $visitor_email \r\n";
+
+//     $headers .= "Reply-To: $visitor_email \r\n";
+
+//     $result = mail($to, $subject, $email_body, $headers);
+
+//     if ($result === false) {
+//         echo 'Mail niet verstuurd';
+//         exit;
+//     }
+
+//     header("Location: index.html");
+//     $message = "De mail is verstuurd!";
+//     echo "<script type='text/javascript'>alert('$message');</script>";
+// }
+?>
+
+
+
+<?php 
 
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $visitor_email = $_POST['email'];
     $message = $_POST['subject'];
 
-    $email_body = "Gebruikers naam: $firstname' '$lastname\n" .
-        "Gebruikers Email: $visitor_email\n" .
-        "Bericht: $message.\n";
+    require_once 'vendor/autoload.php';
+    require_once 'credential.php';
 
+// Create the Transport
+$transport = (new Swift_SmtpTransport('smtp.mailtrap.io', 2525))
+  ->setUsername(EMAIL)
+  ->setPassword(PASS)
+;
 
-    $to = "29118@ma-web.nl";
+// Create the Mailer using your created Transport
+$mailer = new Swift_Mailer($transport);
 
-    $headers = "From: $firstname \r\n";
-    $headers = "From: $lastname \r\n";
-    $headers = "From: $visitor_email \r\n";
+// Create a message
+$message = (new Swift_Message('Byellow' . " " . $firstname . " " . $lastname))
+  ->setFrom(['info@byellow.nl' => 'Teun'])
+  ->setTo([$visitor_email])
+  ->setBody($message . " Naam: " . $firstname . " " . $lastname . "Email: " . $visitor_email)
+  ;
 
-    $headers .= "Reply-To: $visitor_email \r\n";
+// Send the message
 
-    $result = mail($to, $subject, $email_body, $headers);
-
-    if ($result === false) {
-        echo 'Mail niet verstuurd';
-        exit;
-    }
-
+if($mailer->send($message)){
+    header("Location: index.html#form");
+    $end_message = "De mail is verstuurd";
+    echo "<script type='text/javascript'>alert('$end_message');</script>";
+}else{
     header("Location: index.html");
-    $message = "De mail is verstuurd!";
-    echo "<script type='text/javascript'>alert('$message');</script>";
+    $end_message = "De mail is niet verstuurd";
+    echo "<script type='text/javascript'>alert('$end_message');</script>";
 }
+
+
+
+
 ?>
---
+++ credential.php
@@ -0,0 +1,4 @@
+<?php 
+    define('EMAIL', "4cc218f9711d2e");
+    define('PASS', "e34d6f7e00cbf1")
+?>
